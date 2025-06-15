fd1e5eccae826dfa542007a494dfc4e7a6bc1412
Brandon Isaac <isaac.datch22@students.dkut.ac.ke>
2025-06-15 14:57:40 +0300

    Refactor code structure for improved readability and maintainability

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://ec2-16-171-197-114.eu-north-1.compute.amazonaws.com/api',
-  baseUrl: 'http://ec2-16-171-197-114.eu-north-1.compute.amazonaws.com'
+  apiUrl: 'https://employbridge.onrender.com/api',
+  baseUrl: 'https://employbridge.onrender.com'
 };
